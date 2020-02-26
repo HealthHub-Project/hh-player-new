f15b5585cb938ed1ee0faf3b6aef5a476a82723f
joohyukjung <joohyukjung@gmail.com>
2020-02-26 12:58:14 +0900

    none

--- hh-player-new.js
+++ hh-player-new.js
@@ -1,9 +1,9 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import '@polymer/paper-icon-button/paper-icon-button';
-import '@polymer/iron-icons/av-icons';
-import '@polymer/paper-dropdown-menu/paper-dropdown-menu-light';
-import '@polymer/paper-listbox/paper-listbox';
-import '@polymer/paper-item/paper-item';
+import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/iron-icons/av-icons.js';
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu-light.js';
+import '@polymer/paper-listbox/paper-listbox.js';
+import '@polymer/paper-item/paper-item.js';
 import '@polymer/paper-slider/paper-slider';
 
 /**
